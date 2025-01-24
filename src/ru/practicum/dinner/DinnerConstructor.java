69efda87992d5f37385be223a13fc096ce5bd8fe
Oleg Gulyaev <kwwlogin@yandex.ru>
2025-01-24 20:45:27 +0300

    First commit - adding method addDishes

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -1,5 +1,14 @@
 package ru.practicum.dinner;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class DinnerConstructor {
+    Random random = new Random();
+    int randomNumber = random.nextInt(100);
+
+    public addDishes(String dishType, String dishName) {
+
+    }
 
 }
--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -42,6 +42,7 @@ public class Main {
         String dishName = scanner.nextLine();
 
         // добавьте новое блюдо
+        dc.addDishes(dishType, dishName);
     }
 
     private static void generateDishCombo() {
